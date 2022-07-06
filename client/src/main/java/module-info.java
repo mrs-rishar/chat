064b599247afbaa871e2665abe7045e7f3b47d01
AndreiAndroid <exfo@mail.ru>
2022-07-06 14:04:07 +0300

    chat console

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires com.example.network;
 
 
     opens com.example.client to javafx.fxml;
